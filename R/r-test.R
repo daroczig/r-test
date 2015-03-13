a16669f846d5baee2f38fcdadd470e1fa53a7e47
Gergely Daróczi <daroczig@rapporter.net>
2015-03-12 23:46:22 -0700

    readd curly braces

--- R/r-test.R
+++ R/r-test.R
@@ -3,8 +3,9 @@
 #' @return numeric vector
 #' @export
 f <- function(x) {
-    if (is.numeric(x))
+    if (is.numeric(x)) {
         return(x)
-    else
+    } else {
         as.numeric(x)
+    }
 }
